11f85ce42da6e52d911c08b1374f33d8d0b179de
Michael Lindenau <michael.lindenau@flexdrive.com>
2018-03-06 12:13:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Prometheus = require('prom-client')
+const fastify = require('fastify')()
+
+const DEFAULT_HOST = '127.0.0.1'
+const DEFAULT_PORT = 19999
+
+function Monitor(options) {
+  const seneca = this
+  const host = options.host || DEFAULT_HOST
+  const port = options.port || DEFAULT_PORT
+
+  const defaultMetrics = Prometheus.collectDefaultMetrics()
+
+  function expose(port, host) {
+    fastify.get('/metrics', function(request, reply) {
+      reply
+        .header('Content-Type', Prometheus.register.contentType)
+        .send(Prometheus.register.metrics())
+    })
+
+    fastify.listen(port, host, function(err) {
+      if (err) throw err
+      console.log(`server listening on ${fastify.server.address().port}`)
+    })
+  }
+
+  expose(port, host)
+
+  process.on('SIGTERM', () => clearInterval(defaultMetrics))
+}
+
+module.exports = Monitor
